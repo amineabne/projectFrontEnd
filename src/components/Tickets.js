3c493ce9e6dd5107d0237600d18caef5a9d82b9e
amine ab <aminaboulyaktan@gmail.com>
2023-09-22 08:53:44 -0700

    ticketsjs mod

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,130 +1,130 @@
 import React, { useState, useEffect } from 'react';
+import './Tickets.css';
 
 function Tickets() {
-  
   const [tickets, setTickets] = useState([]);
-
+  const [editingTicket, setEditingTicket] = useState(null);
+  const [newTicket, setNewTicket] = useState({
+    eventName: '',
+    eventDate: '',
+    price: '',
+    details: '',
+    state: '',
+  });
 
   useEffect(() => {
-    // URL de l'endpoint
-    const authenticationEndpoint = 'http://localhost:8080/tickets';
+    // Charger les données de billets depuis l'API lorsque le composant est monté
+    fetch('http://localhost:8080/tickets')
+      .then((response) => response.json())
+      .then((data) => setTickets(data))
+      .catch((error) => console.error('Erreur lors du chargement des billets :', error));
+  }, []);
 
-    //Utilisation de fetch pour envoyer la requête 'GET' 
-    fetch(authenticationEndpoint, {
-      method: 'GET',
+  const handleEdit = (ticket) => {
+    setEditingTicket(ticket);
+  };
+
+  const handleDelete = (ticket) => {
+    // Envoyer une requête DELETE à l'API pour supprimer le billet
+    fetch(`http://localhost:8080/tickets/${ticket.id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        // Après la suppression réussie, mettre à jour l'état local "tickets" en excluant le billet supprimé
+        const updatedTickets = tickets.filter((t) => t.id !== ticket.id);
+        setTickets(updatedTickets);
+        setEditingTicket(null);
+      })
+      .catch((error) => console.error('Erreur lors de la suppression du billet :', error));
+  };
+
+  const handleSave = (editedTicket) => {
+    // Envoyer une requête PUT à l'API pour mettre à jour le billet avec les données modifiées
+    fetch(`http://localhost:8080/tickets/${editedTicket.id}`, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdW5haWQudGFsYWF0MTk5MEBnbWFpbC5jb20iLCJpYXQiOjE2OTUxMTgyMjAsImV4cCI6MTY5NTIwNDYyMH0.OToOG0PYx0eNlZYsq_2jxnCLftGWW1_oHMa78UAxLCE'
       },
+      body: JSON.stringify(editedTicket),
     })
-      .then((response) => {
-        console.log("ok") ;
-        
-        if (response.ok) {
-          console.log("ok")
-          return response.json().then((data) => {
-            const { token } = data;
-            localStorage.setItem('jwtToken', token);
-            
-          console.log("ok")
-            setTickets(data);
-           
-          });
-        } else {
-         
-          throw new Error('Erreur lors de la récupération des billets');
-        }
+      .then(() => {
+        // Après la mise à jour réussie, mettre à jour l'état local "tickets" avec la nouvelle version du billet
+        const updatedTickets = tickets.map((t) => (t.id === editedTicket.id ? editedTicket : t));
+        setTickets(updatedTickets);
+        setEditingTicket(null);
       })
-      .catch((error) => {
-    
-        console.error('Erreur :', error.message);
-      });
-  }, []); 
+      .catch((error) => console.error('Erreur lors de la mise à jour du billet :', error));
+  };
+
+  const handleCreate = () => {
+    // Envoyer une requête POST à l'API pour créer un nouveau billet en utilisant les données de "newTicket"
+    fetch('http://localhost:8080/tickets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTicket),
+    })
+      .then(() => {
+        // Après la création réussie, mettre à jour l'état local "tickets" en ajoutant le nouveau billet
+        fetch('http://localhost:8080/tickets')
+          .then((response) => response.json())
+          .then((data) => {
+            setTickets(data);
+            setNewTicket({
+              eventName: '',
+              eventDate: '',
+              price: '',
+              details: '',
+              state: '',
+            });
+          })
+          .catch((error) =>
+            console.error('Erreur lors du chargement des billets après la création :', error)
+          );
+      })
+      .catch((error) => console.error('Erreur lors de la création du billet :', error));
+  };
 
   return (
-
-      <div>
-        <h1> Tickets available </h1>
-        <ul>
+    <div>
+      <h1>Tickets disponibles</h1>
+      <button onClick={handleCreate}>Créer un nouveau billet</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Event Name</th>
+            <th>Event Date</th>
+            <th>Price</th>
+            <th>Details</th>
+            <th>State</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
           {tickets.map((ticket) => (
-            <li key={ticket.id}>
-              <strong>Event Name:</strong> {ticket.eventName}<br />
-              <strong>Event Date:</strong> {new Date(ticket.eventDate).toLocaleDateString()}<br />
-              <strong>Price:</strong> {ticket.price}<br />
-              <strong>Details:</strong> {ticket.details}<br />
-              <strong>State:</strong> {ticket.state}<br />
-            </li>
+            <tr key={ticket.id}>
+              <td>{ticket.eventName}</td>
+              <td>{new Date(ticket.eventDate).toLocaleDateString()}</td>
+              <td>{ticket.price}</td>
+              <td>{ticket.details}</td>
+              <td>{ticket.state}</td>
+              <td>
+                {editingTicket === ticket ? (
+                  <button onClick={() => handleSave(ticket)}>Enregistrer</button>
+                ) : (
+                  <>
+                    <button onClick={() => handleEdit(ticket)}>Éditer</button>
+                    <button onClick={() => handleDelete(ticket)}>Supprimer</button>
+                  </>
+                )}
+              </td>
+            </tr>
           ))}
-        </ul>
-      </div>
-    );
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const [searchResults, setSearchResults] = useState([]);
-//   const [errorMessage, setErrorMessage] = useState('');
-
-//   const handleSearchChange = (e) => {
-//     setSearchTerm(e.target.value);
-//   };
-
-//   const handleSearch = () => {
-//     // Effectuez une requête GET à l'endpoint pour récupérer les billets
-//     const fetchEndpoint = 'http://localhost:8080/users/tickets';
-
-//     fetch(fetchEndpoint)
-//       .then((response) => {
-//         if (response.ok) {
-//           return response.json().then((data) => {
-//             // Stockez les résultats de la recherche dans l'état
-//             setSearchResults(data.billets); // Assurez-vous que les billets sont correctement extraits de la réponse.
-//             setErrorMessage('');
-//           });
-//         } else {
-//           throw new Error('Erreur lors de la recherche');
-//         }
-//       })
-//       .catch((error) => {
-//         console.error('Erreur :', error.message);
-//         setSearchResults([]);
-//         setErrorMessage('Une erreur s\'est produite lors de la recherche');
-//       });
-//   };
-
-//   return (
-//     <div className="p-1 m-1">
-//       <div className="card">
-//         <div className="card-body">
-//           <h3>Billets</h3>
-//           <div className="form-group">
-//             <label htmlFor="search">Rechercher des billets :</label>
-//             <input
-//               type="text"
-//               id="search"
-//               value={searchTerm}
-//               onChange={handleSearchChange}
-//               placeholder="Entrez un terme de recherche"
-//             />
-//             <button type="button" onClick={handleSearch} className="search-button">
-//               Rechercher
-//             </button>
-//           </div>
-//           {errorMessage && <div className="error-message">{errorMessage}</div>}
-//         </div>
-//         {/* Affichez les résultats de la recherche */}
-//         {searchResults.length > 0 && (
-//           <div className="search-results">
-//             <h3>Résultats de la recherche :</h3>
-//             <ul>
-//               {searchResults.map((result) => (
-//                 <li key={result.id}>{result.title}</li>
-//               ))}
-//             </ul>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-//  }
-export default Tickets ;
+export default Tickets;
---
+++ src/components/recherche.js
@@ -0,0 +1,67 @@
+
+const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Effectuez une requête GET à l'endpoint pour récupérer les billets
+    const fetchEndpoint = 'http://localhost:8080/users/tickets';
+
+    fetch(fetchEndpoint)
+      .then((response) => {
+        if (response.ok) {
+          return response.json().then((data) => {
+            // Stockez les résultats de la recherche dans l'état
+            setSearchResults(data.billets); // Assurez-vous que les billets sont correctement extraits de la réponse.
+            setErrorMessage('');
+          });
+        } else {
+          throw new Error('Erreur lors de la recherche');
+        }
+      })
+      .catch((error) => {
+        console.error('Erreur :', error.message);
+        setSearchResults([]);
+        setErrorMessage('Une erreur s\'est produite lors de la recherche');
+      });
+  };
+
+  return (
+    <div className="p-1 m-1">
+      <div className="card">
+        <div className="card-body">
+          <h3>Billets</h3>
+          <div className="form-group">
+            <label htmlFor="search">Rechercher des billets :</label>
+            <input
+              type="text"
+              id="search"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              placeholder="Entrez un terme de recherche"
+            />
+            <button type="button" onClick={handleSearch} className="search-button">
+              Rechercher
+            </button>
+          </div>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+        </div>
+        {/* Affichez les résultats de la recherche */}
+        {searchResults.length > 0 && (
+          <div className="search-results">
+            <h3>Résultats de la recherche :</h3>
+            <ul>
+              {searchResults.map((result) => (
+                <li key={result.id}>{result.title}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+ 
